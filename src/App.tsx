959620bb9d9d9fff8adad867b94f623ff7df33d6
minnyoung <918liter@gmail.com>
2023-05-02 21:22:00 +0900

    feat: 기본 레이아웃 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import Main from './pages/Main';
+
 function App() {
-  return <h1>원티드 프리온보딩 프론트엔드 인턴십(4월) 2주차 기업과제, 7팀</h1>;
+  return <Main />;
 }
 
 export default App;
---
+++ src/pages/Main.tsx
@@ -0,0 +1,13 @@
+export default function Main() {
+  return (
+    <div>
+      <h2>
+        국내 모든 임상시험 검색하고
+        <br />
+        온라인으로 참여하기
+      </h2>
+      <input type="text" />
+      <button>검색</button>
+    </div>
+  );
+}
