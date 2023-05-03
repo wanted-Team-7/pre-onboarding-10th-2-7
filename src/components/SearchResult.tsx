faf490ceeb9b8228772ea932abcdf32a2c5063ff
Jinnie <hjk7639@gmail.com>
2023-05-04 02:31:28 +0900

    feat: SearchResult 컴포넌트에서 요청된 결과가 있을 때 리스트를 뿌려주는 로직 작성

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,9 +1,15 @@
+import SearchResultList from './SearchResultList';
 import { SearchResultArea } from '../style/SearchResult.styled';
+import { SearchResults } from '../types/result';
 
-const SearchResult = () => {
+const SearchResult = ({ searchResult }: { searchResult: SearchResults[] }) => {
   return (
     <SearchResultArea>
       <span>추천 검색어</span>
+      {searchResult &&
+        searchResult.map(result => {
+          return <SearchResultList key={result.id} result={result} />;
+        })}
     </SearchResultArea>
   );
 };
