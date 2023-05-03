c9b29792a03f80f86963bee1375850ee05cae3f1
junyong <49552804+leejy001@users.noreply.github.com>
2023-05-03 19:17:45 +0900

    feat: main label 컴포넌트 구현

--- src/components/MainLabel.tsx
+++ src/components/MainLabel.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import styled from 'styled-components';
 
 function MainLabel() {
-  return <div>MainLabel</div>;
+  return (
+    <Label>
+      국내 모든 임상시험 검색하고
+      <br />
+      온라인으로 참여하기
+    </Label>
+  );
 }
 
 export default MainLabel;
+
+const Label = styled.h2`
+  text-align: center;
+  font-size: 2.125rem;
+  font-weight: 700;
+  letter-spacing: -0.018em;
+  line-height: 1.6;
+  margin-bottom: 40px;
+`;
