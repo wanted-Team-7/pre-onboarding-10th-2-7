3191900e7c7a78dadbf2513b6b6d00c6ac31caef
jeongdopark <jdpark0816@gmail.com>
2023-05-03 19:07:26 +0900

    feat: main 컴포넌트

--- src/page/main.styled.ts
+++ src/page/main.styled.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   width: 100%;
-  height: 500px;
+  height: 100vh;
   background-color: #cae9ff;
   display: flex;
   justify-content: center;
--- src/page/main.tsx
+++ src/page/main.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import { useState } from 'react';
 import { S } from './main.styled';
-import SearchForm from '../components/search/search';
+import InputBox from '../components/inputBox/inputBox';
+import SearchBox from '../components/searchBox/searchBox';
 
 const Main = () => {
+  const [searchList, setSearchList] = useState<[]>([]);
   return (
     <S.Container>
       <S.Title>
         국내 모든 임상시험 검색하고 <br />
         온라인으로 참여하기
       </S.Title>
-      <SearchForm />
+      <InputBox setSearchList={setSearchList} />
+      <SearchBox searchList={searchList} />
     </S.Container>
   );
 };
