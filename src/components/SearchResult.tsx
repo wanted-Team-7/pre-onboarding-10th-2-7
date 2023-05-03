875db4e1594c5d04617231afb7cbd273ac37aae0
Jinnie <hjk7639@gmail.com>
2023-05-03 21:58:32 +0900

    feat: SearchResult 컴포넌트 생성

---
+++ src/components/SearchResult.tsx
@@ -0,0 +1,11 @@
+import { SearchResultArea } from '../style/SearchResult.styled';
+
+const SearchResult = () => {
+  return (
+    <SearchResultArea>
+      <span>추천 검색어</span>
+    </SearchResultArea>
+  );
+};
+
+export default SearchResult;
---
+++ src/style/SearchResult.styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const SearchResultArea = styled.ol`
+  width: 490px;
+  height: fit-content;
+  margin: 10px auto;
+  padding: 20px;
+  background-color: aliceblue;
+  border-radius: 42px;
+  > span {
+    color: #6d7379;
+  }
+`;
