2cf125254f058fafff285ee4280d3a185d7f8323
Jinnie <hjk7639@gmail.com>
2023-05-03 21:59:37 +0900

    feat: SearchResultItem 컴포넌트 생성

---
+++ src/style/SearchResultList.styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SearchList = styled.li`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  margin-top: 20px;
+  > svg {
+    width: 15px;
+  }
+`;
