e9a70f42bf46656c3ed898571af8e88fc9af8474
jeongdopark <jdpark0816@gmail.com>
2023-05-03 19:04:29 +0900

    fix: 입력값이 빈 문자열일 경우 & console.info 추가

--- src/api/client.js
+++ src/api/client.js
@@ -2,5 +2,7 @@ import axios from 'axios';
 import { API_URL } from '../constants';
 
 export const getSearchWord = async word => {
+  if (word === '') return { data: [] };
+  console.info('calling api');
   return axios.get(API_URL, { params: { name: word } });
 };
