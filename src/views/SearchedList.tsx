7516c51740e36bc473a9f0654f71ca7f0aaa075e
1yoouoo <1yoouoo@gmail.com>
2023-05-04 12:11:55 +0900

    refactor: refactored code in SearchedList component

--- src/views/SearchedList.tsx
+++ src/views/SearchedList.tsx
@@ -1,15 +1,14 @@
-/* eslint-disable no-unused-vars */
 import styled from 'styled-components';
-import SearchedItem from './SearchedItem';
-import { SmallSearchIcon } from '../assets/SearchSVG';
 import { SearchedResponseItem } from '../App';
-import { MAX_SEARCHED_RESULT_NUM } from '../utils/constants';
+import RecentSearches from './RecentSearches';
+import RecommendedSearches from './RecommendedSearches';
+import SearchItem from './SearchItem';
 
 interface SearchedListProps {
   searchedResponse: SearchedResponseItem[];
   searchQuery: string;
   isSearching: boolean;
-  selectedItemIndex: any;
+  selectedItemIndex: number;
 }
 
 const SearchedList = ({
@@ -19,49 +18,22 @@ const SearchedList = ({
   selectedItemIndex,
 }: SearchedListProps) => {
   const isSearchEmpty = searchQuery.trim() === '';
-  const hasSearchedResponse = searchedResponse.length;
   const recentSearches = JSON.parse(localStorage.getItem('recentSearches') || '[]');
 
   return (
     <StyledLayout>
       <StyledUl>
         {isSearchEmpty ? (
-          <>
-            <StyledText>최근 검색어</StyledText>
-            {recentSearches.slice(0, MAX_SEARCHED_RESULT_NUM).map((item: string, index: number) => (
-              <SearchedItem key={index} id={index} name={item} searchQuery={searchQuery} />
-            ))}
-          </>
+          <RecentSearches searchQuery={searchQuery} recentSearches={recentSearches} />
         ) : (
           <>
-            <StyledDiv>
-              <SmallSearchIcon />
-              <StyledLi>
-                <BoldText>{searchQuery}</BoldText>
-              </StyledLi>
-            </StyledDiv>
-            <StyledText>추천 검색어</StyledText>
-            {isSearching ? (
-              <StyledText>검색중 ...</StyledText>
-            ) : (
-              <>
-                {hasSearchedResponse ? (
-                  searchedResponse
-                    .slice(0, MAX_SEARCHED_RESULT_NUM)
-                    .map((item: SearchedResponseItem, index: number) => (
-                      <SearchedItem
-                        key={index}
-                        id={index}
-                        name={item.name}
-                        searchQuery={searchQuery}
-                        isSelected={index === selectedItemIndex}
-                      />
-                    ))
-                ) : (
-                  <StyledText>검색 결과가 없습니다.</StyledText>
-                )}
-              </>
-            )}
+            <SearchItem searchQuery={searchQuery} />
+            <RecommendedSearches
+              searchQuery={searchQuery}
+              isSearching={isSearching}
+              searchedResponse={searchedResponse}
+              selectedItemIndex={selectedItemIndex}
+            />
           </>
         )}
       </StyledUl>
@@ -69,16 +41,21 @@ const SearchedList = ({
   );
 };
 
-const StyledText = styled.div`
-  font-size: 14px;
-  margin: 10px 30px;
-`;
-
 const StyledLayout = styled.div`
   background-color: #fff;
   width: 100%;
   border-radius: 24px;
   margin: 10px;
+`;
+
+const StyledUl = styled.ul`
+  padding: 30px 0;
+  overflow: hidden;
+`;
+
+export const StyledText = styled.div`
+  font-size: 14px;
+  margin: 10px 30px;
 `;
 
 export const StyledDiv = styled.div<{ isSelected?: boolean }>`
@@ -93,14 +70,10 @@ export const StyledDiv = styled.div<{ isSelected?: boolean }>`
   }
 `;
 
-const StyledUl = styled.ul`
-  padding: 30px 0;
-  overflow: hidden;
-`;
-
 export const StyledLi = styled.li`
   margin: 18px;
 `;
+
 export const BoldText = styled.span`
   font-weight: bold;
   color: #e42828;
