49395f2868ed4a25f70898957f0c5d44763aa63f
minnyoung <918liter@gmail.com>
2023-05-05 00:38:39 +0900

    refactor: svg 태그 속성명 변경

--- src/components/SearchSection.tsx
+++ src/components/SearchSection.tsx
@@ -66,8 +66,8 @@ export default function SearchSection({
             <span onClick={handledSearchInputClear}>
               <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" color="white">
                 <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
                   d="M5 6.414l3.293 3.293 1.414-1.414L6.414 5l3.293-3.293L8.293.293 5 3.586 1.707.293.293 1.707 3.586 5 .293 8.293l1.414 1.414L5 6.414z"
                 ></path>
               </svg>
