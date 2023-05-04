1985b75b2893268d91b13234fb2010ee13a7cb47
Jinnie <hjk7639@gmail.com>
2023-05-04 12:01:09 +0900

    feat: 검색 결과를 캐싱하고 api 요청을 보내기 전 캐시된 데이터가 있으면 캐시된 데이터를 리턴하는 로직 작성

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -1,17 +1,33 @@
 import axios from 'axios';
 
+const CAHCE_NAME = 'search-result';
+
 export const getSearchResult = async (word: string) => {
+  const URL = `/api/v1/search-conditions/?name=${word}`;
+  const cachedData = await caches.match(URL);
+
   if (word.trim().length === 0) return [];
 
+  if (cachedData) {
+    const cachedDataList = await cachedData.json();
+    return cachedDataList.slice(0, 7);
+  }
+
   try {
-    const res = await axios.get(`/api/v1/search-conditions/?name=${word}`);
+    const res = await axios.get(URL);
+
+    console.info('calling api', res);
 
     if (res.status !== 200) return;
+
+    caches.open(CAHCE_NAME).then(cache => {
+      cache.add(URL);
+    });
 
     const result = res.data.slice(0, 7);
 
     return result;
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
