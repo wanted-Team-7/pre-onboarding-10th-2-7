52557ca7efd433b5a79b941234bf19d9518a4e31
Jinnie <hjk7639@gmail.com>
2023-05-04 16:35:55 +0900

    feat: 방향키 이벤트 함수 생성, useRef로 검색결과 리스트 요소 선택 방향키 이벤트 함수 SearchInput 컴포넌트로 전달, ref와 인덱스 state를 사용하는 컴포넌트로 전달

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import SearchInputComponent from './components/SearchInput';
 import SearchResult from './components/SearchResult';
 import { SearchResults } from './types/result';
 
 function App() {
   const [searchResult, setSearchResult] = useState<SearchResults[]>([]);
+  const [focusIndex, setFocusIndex] = useState<number>(-1);
+  const focusRef = useRef<HTMLOListElement>(null);
+
+  const keydownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    switch (e.key) {
+      case 'ArrowUp':
+        e.preventDefault();
+        setFocusIndex(prevIndex => (prevIndex <= 0 ? searchResult.length - 1 : prevIndex - 1));
+        break;
+      case 'ArrowDown':
+        e.preventDefault();
+        setFocusIndex(prevIndex => (prevIndex >= searchResult.length - 1 ? 0 : prevIndex + 1));
+        break;
+      case 'Enter':
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <>
-      <SearchInputComponent setSearchResult={setSearchResult} />
-      {searchResult.length > 1 ? <SearchResult searchResult={searchResult} /> : null}
+      <SearchInputComponent setSearchResult={setSearchResult} keydownHandler={keydownHandler} />
+      {searchResult.length > 0 ? (
+        <SearchResult searchResult={searchResult} focusRef={focusRef} focusIndex={focusIndex} />
+      ) : null}
     </>
   );
 }
